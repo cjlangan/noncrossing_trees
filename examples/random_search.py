b1f8fd73180bf75945df21e25fadd3128401f84f
cjlangan <connorjameslangan@gmail.com>
2025-06-20 18:50:52 -0500

    wip: better random tree with k borders retriever

--- examples/random_search.py
+++ examples/random_search.py
@@ -1,16 +1,20 @@
 from ..analysis import ParallelGammaSearcher
+from ..visualization import Visualizer
+from ..generation import NCSTGenerator
 
 def random_search_demo():
     parallel_searcher = ParallelGammaSearcher()
 
-    parallel_searcher.find_trees_with_gamma_parallel(
-            13,     # number of vertices
-            .58,   # gamma value we want, or better
-            "f",    # tree operation applied to second tree (flip)
-            k = 3,  # number of border edges (optional)
-            notable = True,     # option to print notable values
-            plot = True         # option to plot graphs when done
-    )
+    # parallel_searcher.find_trees_with_gamma_parallel(
+    #         20,     # number of vertices
+    #         .499,   # gamma value we want, or better
+    #         "r",    # tree operation applied to second tree (flip)
+    #         k = 4,  # number of border edges (optional)
+    #         notable = True,     # option to print notable values
+    #         plot = True         # option to plot graphs when done
+    # )
+
+    Visualizer.print_tree(NCSTGenerator.generate_ncst_with_k_borders(6, 2)[0])
 
 if __name__ == "__main__":
     random_search_demo()
